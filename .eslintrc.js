759bae4b18cf3df73043c3a2f30473ec9c0f1a96
Cyril PASCAL <cyril.pascal_github@m4x.org>
2020-09-29 15:18:26 +0200

    Use activeTab permission with dynamic script loading

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,13 +21,27 @@ module.exports = {
     'comma-dangle': [
       'error',
       {
-        'arrays': 'always-multiline',
-        'objects': 'always-multiline',
-        'functions': 'never'
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        functions: 'never'
       }
     ],
     'no-cond-assign': ['error', 'always'],
-    'max-len': ["error", {"code": 140}],
+    'max-len': ["error", {code: 140}],
     'no-console': 2,
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'template-curly-spacing': ['error', 'never'],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'key-spacing': ['error', {mode: 'strict'}],
+    'spaced-comment': ['error', 'always'],
+    'no-multiple-empty-lines': ['error', {max: 1,maxEOF: 0 }],
+    'space-infix-ops': ['error'],
+    'multiline-comment-style': ["error", "starred-block"],
+    'block-spacing': ['error', 'always'],
+    'prefer-const': ['error'],
+    'no-const-assign': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'quote-props': ['error', 'consistent-as-needed'],
   },
 };
